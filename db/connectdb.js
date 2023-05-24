d598dd53a90a66d847169a5cd5d8109b42d862af
131441456 <shivanibansal8962@gmail.com>
2023-05-24 21:42:50 +0530

    change_database

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const url = "mongodb://127.0.0.1:27017/admissionportal"
+const live_url = "mongodb+srv://shivani123:<shivani123>@cluster0.z5d3eek.mongodb.net/"
 
 const connectdb = ()=>{
     // For local DB
-    return mongoose.connect(url)
+    return mongoose.connect(live_url)
     // For cloud DB
     // return mongoose.connect(database)
     
