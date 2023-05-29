1f09aec0f1185f00223cb13f9d4eba596665aca1
131441456 <shivanibansal8962@gmail.com>
2023-05-29 21:26:28 +0530

    db_file

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const url = "mongodb://127.0.0.1:27017/admissionportal"
-const live_url = "mongodb+srv://shivani123:shivani123@cluster0.z5d3eek.mongodb.net/addminsionportal?retryWrites=true&w=majority"
+const live_url = "mongodb+srv://shivanibansal:shivani123@cluster0.brlsgvm.mongodb.net/addminsionportal?retryWrites=true&w=majority"
 
 const connectdb = ()=>{
     // For local DB
